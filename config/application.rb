443e8d4927c1cd493c58787a3b275e53aad457df
McN33ly <rmcneely23@gmail.com>
2016-08-10 12:01:31 -0400

    Cleaned up some spaghetti code left over from implementing Bower. Modified GitIgnore to not include NodeModules this time! Check to merge back into Master after this.

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module AngularRailsApp
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets", "stylesheets")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","fonts")
+    # config.assets.paths << Rails.root.join("lib","assets","bower_components","bootstrap-sass-official", "assets", "stylesheets")
+    # config.assets.paths << Rails.root.join("vendor","assets","bower_components","fonts")
     config.assets.paths << Rails.root.join('bower_components')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
