3f608e52d619cb99a3fab63c2c1e9f1214aae445
Luke <github email address>
2016-09-16 08:41:18 -0700

    fixed routes

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -5,59 +5,59 @@ angular
   .config(function($stateProvider, $urlRouterProvider){
     $stateProvider
       .state('home', {
-        url: '/home',
+        url: '/',
         controller:'HomeController',
         controllerAs: 'ctrl',
         templateUrl: 'home.html'
       })
       .state('home.about', {
-        url: '/about',
+        url: 'about',
         controller: 'SkillController',
         controllerAs: 'ctrl',
         templateUrl: 'about.html'
       })
       .state('home.skill', {
-        url: '/skills',
+        url: 'skills',
         controller: 'SkillController as ctrl',
         templateUrl: 'skills/skills.html'
       })
       .state('skill', {
-        url: '/skills/:id',
+        url: 'skills/:id',
         controller: 'SkillController as ctrl',
         templateUrl: 'skills/show.html'
       })
       .state('skill.resources', {
-        url: '/resources',
+        url: 'resources',
         controller: 'ResourceController',
         controllerAs: 'ctrl',
         templateUrl: 'resources/show.html'
       })
       .state('home.skill.new', {
-        url: '/new',
+        url: 'new',
         controller: 'NewSkillController',
         controllerAs: 'ctrl',
         templateUrl: 'skills/new.html'
       })
       .state('home.resources', {
-        url: '/resources',
+        url: 'resources',
         controller: 'ResourceController',
         controllerAs: 'ctrl',
         templateUrl: 'resources/resources.html'
       })
       .state('resource', {
-        url: '/resources/:id',
+        url: 'resources/:id',
         controller: 'ResourceController as ctrl',
         templateUrl: 'resources/show.html'
       })
       .state('home.resources.new', {
-        url: '/new',
+        url: 'new',
         controller: 'NewResourceController',
         controllerAs: 'ctrl',
         templateUrl: 'resources/new.html'
       })
 
       //Default Fallback Route
-      $urlRouterProvider.otherwise('home')
+      $urlRouterProvider.otherwise('/')
 
   //  // enable HTML5 Mode for SEO
   //  $locationProvider.html5Mode({enabled: true, requireBase: false});
